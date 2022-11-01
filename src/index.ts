f0c6287120163da50f2f53e200128b6ccc194d86
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-10-31 19:02:53 -0500

    Update: White Dwarf Core Renamed RenderSystem2DRegister.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { mainWorld } from "../white-dwarf/src/Core";
 import { coreRenderContext } from "../white-dwarf/src/Core/Context/RenderContext";
 import { systemContext } from "../white-dwarf/src/Core/CoreSetup";
-import { RenderSystem2DRegister } from "../white-dwarf/src/Core/Render/RenderSystemRegister";
+import { RenderSystem2DRegister } from "../white-dwarf/src/Core/Render/RenderSystem2DRegister";
 
 export const main = () => {
   systemContext.coreSetup = () => {
